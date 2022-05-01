018b7ce7c3ba834705726ad3e958b173113d475b
stevenmcsorley <smcsorl@gmail.com>
2022-05-01 01:30:23 +0100

    added product pages and routes

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,35 +1,45 @@
 import { Container, CssBaseline } from "@mui/material";
-import createTheme from '@mui/material/styles/createTheme';
-import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import createTheme from "@mui/material/styles/createTheme";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "../../feature/about/about";
 import Catalog from "../../feature/catalog/Catalog";
+import ProductDetails from "../../feature/catalog/ProductDetails";
+import ContactPage from "../../feature/contact/ContactPage";
+import HomePage from "../../feature/home/HomePage";
 import Header from "./Header";
 
 function App() {
-
   const [darkMode, setDarkMode] = useState(false);
-  const paletteType = darkMode ? 'dark' : 'light'
+  const paletteType = darkMode ? "dark" : "light";
   const theme = createTheme({
     palette: {
       mode: paletteType,
       background: {
-        default: paletteType === 'light' ? '#eqeqeq' : '#121212'
-      }
+        default: paletteType === "light" ? "#eqeqeq" : "#121212",
+      },
     },
   });
 
-  function handleThemeChange(){
+  function handleThemeChange() {
     setDarkMode(!darkMode);
   }
 
   return (
     <>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
-      <Container>
-        <Catalog />
-      </Container>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/catalog/:id" element={<ProductDetails />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </Container>
       </ThemeProvider>
     </>
   );
